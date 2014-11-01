2fbd1cb54be2552f06dce42e68c2f2ad843a4a8f
Eugene Kotsyubinsky <turboopera@mail.ru>
2014-11-01 22:11:22 +0200

    fix

--- src/main/java/ua/company/gwt/server/mvc/LoginController.java
+++ src/main/java/ua/company/gwt/server/mvc/LoginController.java
@@ -12,8 +12,6 @@ public class LoginController {
     @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
     public ModelAndView welcome() {
         boolean isAuthenticated = SecurityContextHolder.getContext().getAuthentication().isAuthenticated();
-        System.out.println("-12--1-2-31=2-3=123-=12-3=12-3=12-312=3213");
-//        return new ModelAndView("/loginPage");
         if (isAuthenticated) {
             return new ModelAndView("redirect:/gwt.html");
         } else {
